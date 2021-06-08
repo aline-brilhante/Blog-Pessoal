b5f5749f596c954f34f968a3f3dbd659b05a0cec
aline-brilhante <lykarms@gmail.com>
2021-06-08 11:22:17 -0300

    correção UsuarioService

--- blogPessoal/blogPessoal/src/main/java/com/blogpessoal/blogPessoal/service/UsuarioService.java
+++ blogPessoal/blogPessoal/src/main/java/com/blogpessoal/blogPessoal/service/UsuarioService.java
@@ -41,7 +41,7 @@ public class UsuarioService {
 				user.get().setToken(authHeader);
 				user.get().setNome(usuario.get().getNome());
 				
-				return user;
+				return Optional.empty();
 				
 			}
 		}
